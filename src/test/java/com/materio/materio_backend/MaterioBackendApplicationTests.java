ef9ab0a83c39fba2687a6af15559f4c113330c6f
LionelB79 <110908951+LionelB79@users.noreply.github.com>
2025-02-06 16:41:19 +0100

    generation de l'app de base

---
+++ src/test/java/com/materio/materio_backend/MaterioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.materio.materio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
