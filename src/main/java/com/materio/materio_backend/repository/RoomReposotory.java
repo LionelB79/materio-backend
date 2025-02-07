5955976095023354c385d84877e34f3eef79693c
LionelB79 <110908951+LionelB79@users.noreply.github.com>
2025-02-07 10:39:41 +0100

    ajout RoomRepo

---
+++ src/main/java/com/materio/materio_backend/repository/RoomReposotory.java
@@ -0,0 +1,6 @@
+package com.materio.materio_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomReposotory extends JpaRepository<RoomReposotory,Long> {
+}
