23027d94a2325fd51984f03536853c1fd3b4fc1b
LionelB79 <110908951+LionelB79@users.noreply.github.com>
2025-02-13 16:34:43 +0100

    ajout exceptions spécifiques

---
+++ src/main/java/com/materio/materio_backend/business/exception/DuplicateEquipmentException.java
@@ -0,0 +1,7 @@
+package com.materio.materio_backend.business.exception;
+
+public class DuplicateEquipmentException extends RuntimeException {
+    public DuplicateEquipmentException(String referenceName, String serialNumber) {
+        super(String.format("L'équipement %s avec le numéro de série %s existe déjà", referenceName, serialNumber));
+    }
+}
--- src/main/java/com/materio/materio_backend/business/exception/GlobalExceptionHandler.java
+++ src/main/java/com/materio/materio_backend/business/exception/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package com.materio.materio_backend.business.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(RoomNotFoundException.class)
@@ -40,13 +42,31 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(LocalityNotFoundException.class)
     ProblemDetail handleLocalityNotFoundException(LocalityNotFoundException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
-                HttpStatus.CONFLICT,
+                HttpStatus.NOT_FOUND,
                 e.getMessage()
         );
-        problemDetail.setTitle("Localisation d'équipement incorrecte");
+        problemDetail.setTitle("Localité introuvable");
         return problemDetail;
     }
 
+    @ExceptionHandler(DuplicateEquipmentException.class)
+    ProblemDetail handleDuplicateEquipmentException(DuplicateEquipmentException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.CONFLICT,
+                e.getMessage()
+        );
+        problemDetail.setTitle("Equipment déjà existant");
+        return problemDetail;
+    }
 
+    @ExceptionHandler(DuplicateLocalityException.class)
+    ProblemDetail handleDuplicateLocalityException(DuplicateLocalityException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.CONFLICT,
+                e.getMessage()
+        );
+        problemDetail.setTitle("Localité déjà existante");
+        return problemDetail;
+    }
 
 }
